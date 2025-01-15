510ef1ccd23b86f7b9f7a730b6e21934db07d1d2
shudd3r <q3.shudder@gmail.com>
2025-01-15 20:54:10 +0100

    Added default ordered class elements fixer setup

--- src/FixerFactory.php
+++ src/FixerFactory.php
@@ -50,7 +50,7 @@ final class FixerFactory
         'no_useless_else'                       => true,
         'no_useless_return'                     => true,
         'non_printable_character'               => ['use_escape_sequences_in_strings' => true],
-        'ordered_class_elements'                => true,
+        'ordered_class_elements'                => ['order' => [], 'sort_algorithm' => 'none'],
         'ordered_imports'                       => false,
         'php_unit_strict'                       => false,
         'php_unit_method_casing'                => false,
@@ -82,6 +82,14 @@ final class FixerFactory
         self::$rules['no_extra_blank_lines']['tokens'] = [
             'break', 'continue', 'extra', 'return', 'throw', 'parenthesis_brace_block',
             'square_brace_block', 'curly_brace_block'
+        ];
+
+        self::$rules['ordered_class_elements']['order'] = [
+            'use_trait', 'case',
+            'constant_public', 'constant_protected', 'constant_private',
+            'property_public', 'property_protected', 'property_private',
+            'construct', 'destruct', 'magic', 'phpunit',
+            'method_public', 'method_protected', 'method_private'
         ];
 
         self::$rules['Polymorphine/double_line_before_class_definition']     = true;
---
+++ tests/Fixtures/code-samples/Fixer/expected-ClassOrder.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Polymorphine/Dev package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Dev\CodeSamples\Fixer;
+
+
+class ClassOrder
+{
+    public const CONST_VALUE    = 'value';
+    public const PUB_CONS_VALUE = 'pub-value';
+
+    private const PRIVATE_CONS_VALUE = 'private-value';
+
+    public static $publicStatic;
+
+    public ?string $publicValue = null;
+
+    protected static $staticValue;
+
+    private $value;
+
+    public function __construct()
+    {
+    }
+
+    public static function instance(): self
+    {
+        return new self();
+    }
+
+    public function setUpBeforeClass(): void
+    {
+    }
+
+    private function setUp()
+    {
+    }
+
+    public static function doPublicStatic(): void
+    {
+    }
+
+    public function doPublic(): void
+    {
+    }
+
+    protected function doProtected()
+    {
+    }
+
+    protected static function doProtectedStatic(): int
+    {
+        return 1;
+    }
+
+    private function doPrivate()
+    {
+    }
+}
