ce81d963bcecb8d904cfa4b15d11558c1231ad0b
shudd3r <q3.shudder@gmail.com>
2026-09-11 05:37:50 +0200

    Moved CLI args to CodeSniffer ruleset

--- src/Tools/SnifferTokens.php
+++ src/Tools/SnifferTokens.php
@@ -35,7 +35,7 @@ final class SnifferTokens
      */
     public static function runner(?string $configFile = null): Runner
     {
-        $configFile = $configFile ?: dirname(__DIR__, 2) . '/phpcs.xml.dist';
+        $configFile = $configFile ?: dirname(__DIR__, 2) . '/phpcs.self.xml';
         $runner     = new Runner();
         $runner->config = new Config(['-q', '--standard=' . $configFile]);
         $runner->init();
